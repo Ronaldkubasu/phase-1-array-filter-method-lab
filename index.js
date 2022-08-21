55025909de437787e44cfa18a10b1ae51da1747a
Ronald kubasu <ronald.kubasu@student.moringaschool.com>
2022-08-21 04:32:36 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+let findMatching=function(array,string){
+    return array.filter(s=>s.toLowerCase()==string.toLowerCase())
+}
+
+let fuzzyMatch = function(array,string){
+    return array.filter(item=>item[0] == string[0])
+}
+
+let matchName = function(array,string){
+    return array.filter(item=>item.name==string)
+}
